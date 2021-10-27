14ce368b032eac57456c5baaed8230f569b6a4df
Ankan Sikdar <ankansikdar@gmail.com>
2021-10-27 20:22:03 +0530

    Created widgets

---
+++ lib/widgets/content.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Content extends StatelessWidget {
+  final String text;
+  const Content({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 8.0),
+      child: SelectableText(
+        text,
+        style: const TextStyle(fontSize: 18.0),
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+}
---
+++ lib/widgets/open_link.dart
@@ -0,0 +1,31 @@
+import 'dart:js' as js;
+import 'package:flutter/material.dart';
+
+class OpenLink extends StatelessWidget {
+  final String text;
+  final String url;
+  const OpenLink({
+    Key? key,
+    required this.text,
+    required this.url,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: InkWell(
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 18.0,
+            color: Colors.blue,
+          ),
+        ),
+        onTap: () {
+          js.context.callMethod('open', [url]);
+        },
+      ),
+    );
+  }
+}
