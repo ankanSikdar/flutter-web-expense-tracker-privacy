edc038c5a7b92fb34290afd06b6dadc904f3fa51
Ankan Sikdar <ankansikdar@gmail.com>
2021-10-27 20:44:37 +0530

    Added cookies

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,7 @@ class MyHomePage extends StatelessWidget {
             PrivacyComponent(),
             InfoCollected(),
             LogData(),
+            Cookies(),
           ],
         ),
       ),
